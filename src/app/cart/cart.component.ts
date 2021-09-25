1bb7205f3f135b7d425452912d5447ce344c1c0c
Bence <bauerbence5@gmail.com>
2021-09-25 18:39:09 +0200

    started cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../models/Product';
+import { ProductsService } from '../services/products.service';
 
 @Component({
   selector: 'app-cart',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+  productList: Product[] = [];
+  cartPrice: number = 0;
 
-  constructor() { }
+  constructor(private productsService: ProductsService) { }
 
   ngOnInit(): void {
+    this.productList = this.productsService.getProducts();//.filter(p => p.quantity > 0);
+    this.calculatePrice();
+  }
+
+  calculatePrice() {
+    this.cartPrice = 0;
+    this.productList.map(p => {
+      this.cartPrice += p.price * p.quantity;
+    });
+    this.cartPrice = Number(this.cartPrice.toFixed(2));
   }
 
 }
