f9da280554d053b5aa90b89e09f12fca5270d787
Bence <bauerbence5@gmail.com>
2021-09-25 12:23:49 +0200

    created product model

---
+++ src/app/models/Product.ts
@@ -0,0 +1,13 @@
+export class Product {
+    id: number;
+    name: string;
+    price: number;
+    quantity: number;
+
+    constructor() {
+        this.id=1;
+        this.name='';
+        this.price=1.0;
+        this.quantity=1;
+    }
+}
